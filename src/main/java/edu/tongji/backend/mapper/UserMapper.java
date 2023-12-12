bc6d12fe7644d13a996c261d938e95a0c57bdd11
Victor Hu <victorhu493@gmail.com>
2023-12-13 00:26:41 +0800

    配置Github Secrets与Github Action (#1)
    注意resources的mapper的相对路径需要与对应的接口文件一致，一般直接使用*Mapper.xml是不起作用的

--- src/main/java/edu/tongji/backend/mapper/UserMapper.java
+++ src/main/java/edu/tongji/backend/mapper/UserMapper.java
@@ -7,5 +7,6 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper extends BaseMapper<user> {
     @Select("select * from user where name= #{name};")
-    user select1(String name);//自定义函数，通过SQL语句进行查询。本函数仅做示例，可能没有用
+    user select1(String name);//自定义函数，通过SQL语句进行查询。本函数仅做示例，可能没有用gi
+    user selectById(int id);
 }
