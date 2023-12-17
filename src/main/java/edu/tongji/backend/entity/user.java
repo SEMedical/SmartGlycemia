7e6d0a546e37c35229adc323c906663135b0092d
Victor Hu <victorhu493@gmail.com>
2023-12-18 00:44:17 +0800

    Update user.java

--- src/main/java/edu/tongji/backend/entity/user.java
+++ src/main/java/edu/tongji/backend/entity/user.java
@@ -1,5 +1,6 @@
 package edu.tongji.backend.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor//自动完成有参构造
 @NoArgsConstructor//自动完成无参构造
 public class user {
+    @TableId
     private Long user_id;//需要与表格里的属性对应
     private Integer age;
     private String name;
