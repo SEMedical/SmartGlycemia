27f740e581fe1fb1c15f950e549c9dba6c34c1b5
Victor Hu <victorhu493@gmail.com>
2023-12-20 00:21:01 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 start=$(date +"%s")
 
 ssh -v -p ${SERVER_PORT} ${SERVER_USER}@${SERVER_HOST} 
-ifconfig
+
 docker pull victor005/tangxiaozhi_sports_backend:latest
 
 CONTAINER_NAME=glycemiaapp
@@ -13,7 +13,7 @@ if [ "$(docker ps -qa -f name=$CONTAINER_NAME)" ]; then
         docker stop $CONTAINER_NAME;
     fi
 fi
-
+ifconfig
 docker run -d --rm -p 8000:8000 --name $CONTAINER_NAME victor005/tangxiaozhi_sports_backend:latest
 
 exit
