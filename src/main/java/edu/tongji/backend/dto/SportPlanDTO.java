92a121f64d7b9156572953369541da71af065384
UltraTempest10 <104293424+UltraTempest10@users.noreply.github.com>
2024-01-01 22:14:43 +0800

    问卷功能

---
+++ src/main/java/edu/tongji/backend/dto/SportPlanDTO.java
@@ -0,0 +1,14 @@
+package edu.tongji.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SportPlanDTO {
+    private List<Sport> scenario;
+}
---
+++ src/main/java/edu/tongji/backend/service/IQuestionnaireService.java
@@ -0,0 +1,15 @@
+package edu.tongji.backend.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.tongji.backend.dto.Answer2;
+import edu.tongji.backend.dto.QuestionnaireDTO;
+import edu.tongji.backend.dto.SportPlanDTO;
+import edu.tongji.backend.entity.Questionnaire;
+
+import java.util.List;
+
+public interface IQuestionnaireService extends IService<Questionnaire> {
+    List<String> getQuestionnaire1();
+    List<QuestionnaireDTO> getQuestionnaire2(Integer userId, List<Boolean> answer);
+    SportPlanDTO getRecommendedSportPlan(Integer userId, List<Answer2> answer);
+}
