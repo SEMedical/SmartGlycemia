4bc56a095677eaf06c3c6110734736c088b08d73
UltraTempest10 <104293424+UltraTempest10@users.noreply.github.com>
2024-01-01 23:01:39 +0800

    问卷功能

---
+++ src/main/java/edu/tongji/backend/dto/Answer2DTO.java
@@ -0,0 +1,15 @@
+package edu.tongji.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Answer2DTO {
+    private List<Answer2> result;
+}
+
