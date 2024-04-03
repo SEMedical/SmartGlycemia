6f0e7ddfad51d645721dfa72fca8d5b2a53fe6f0
Victor Hu <victorhu493@gmail.com>
2024-04-03 16:44:38 +0800

    ensure atomicity via lua script when unlock the distributed lock

--- src/main/java/edu/tongji/backend/util/SimpleRedisLock.java
+++ src/main/java/edu/tongji/backend/util/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package edu.tongji.backend.util;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -15,11 +19,17 @@ public class SimpleRedisLock implements ILock{
 
     private RedisTemplate redisTemplate;
     private static final String KEY_PREFIX="lock:";
-
+    private static final String ID_PREFIX=UUID.randomUUID()+"-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT=new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     @Override
     public boolean tryLock(long timeoutSec) {
         //Get the Id of currentthread
-        Long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX+Thread.currentThread().getId();
         Boolean success=redisTemplate.opsForValue().
                 setIfAbsent(KEY_PREFIX+name,threadId.toString(),timeoutSec,
                 TimeUnit.SECONDS);//setnx
@@ -29,7 +39,11 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
-        //Release
-        redisTemplate.delete(KEY_PREFIX+name);
+
+        redisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX+name),
+                ID_PREFIX+Thread.currentThread().getId()
+        );
     }
 }
