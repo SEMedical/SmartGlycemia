2704d67ab048b8e1bb7c2cc3a418b2a860e61e52
Victor Hu <victorhu493@gmail.com>
2024-04-18 23:10:39 +0800

    fix the overwhelm scope for gateway module(web module can't be imported)

--- common-api/src/main/java/edu/tongji/backend/config/MvcConfig.java
+++ common-api/src/main/java/edu/tongji/backend/config/MvcConfig.java
@@ -4,7 +4,6 @@ import edu.tongji.backend.util.LoginInterceptor;
 import edu.tongji.backend.util.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- gateway/src/main/java/edu/tongji/backend/GatewayApplication.java
+++ gateway/src/main/java/edu/tongji/backend/GatewayApplication.java
@@ -2,7 +2,6 @@ package edu.tongji.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
