03466813c718e1a0a4e6c64c1aaa4b9fc50c7db1
Victor Hu <victorhu493@gmail.com>
2024-04-22 20:31:56 +0800

    add jvm exporter for survilliance

--- user-service/src/main/java/edu/tongji/backend/UserApplication.java
+++ user-service/src/main/java/edu/tongji/backend/UserApplication.java
@@ -1,15 +1,15 @@
 package edu.tongji.backend;
-import com.alibaba.nacos.api.annotation.NacosProperties;
-import com.alibaba.nacos.api.config.annotation.NacosProperty;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @EnableFeignClients
 @SpringBootApplication
 @MapperScan("edu.tongji.backend.mapper")
-//@NacosPropertySource(dataId = "security.yml")
+@Slf4j
 public class UserApplication {
 
     public static void main(String[] args) {
