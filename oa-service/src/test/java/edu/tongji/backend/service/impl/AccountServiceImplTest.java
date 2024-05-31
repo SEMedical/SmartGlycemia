2b5e207c3069256397ff377e4ed224cdec4fc688
rmEleven <1569323975@qq.com>
2024-05-31 12:30:11 +0800

    create new branch

---
+++ oa-service/src/test/java/edu/tongji/backend/service/impl/AccountServiceImplTest.java
@@ -0,0 +1,24 @@
+package edu.tongji.backend.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceImplTest {
+
+    @Test
+    void getAccountList() {
+    }
+
+    @Test
+    void addAccount() {
+    }
+
+    @Test
+    void deleteAccount() {
+    }
+
+    @Test
+    void repeatedIdCard() {
+    }
+}
