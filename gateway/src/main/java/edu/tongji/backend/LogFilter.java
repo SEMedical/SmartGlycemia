fd33981120421be7aa9aa1edd9ba6e225f451ac0
Victor Hu <victorhu493@gmail.com>
2024-06-22 18:01:32 +0800

    Nginx and MySQL Images experiment are successful! NOTE: 1. MySQL Schema not only can be restored by data/,but also by SQL Scripts located at #DB Repo. 2. Redis can be restored by AOF&RDB stored under /tmp/redis/data 3. Nginx Configuration files can be merged as one!Don't forget the mapping of ports,and the name must be nginx.conf.(Nacos configuration center will be removed in near future) 4. You can put the dist packages at /tmp/nginx/html/{admin or doctor}

--- gateway/src/main/java/edu/tongji/backend/LogFilter.java
+++ gateway/src/main/java/edu/tongji/backend/LogFilter.java
@@ -1,5 +1,6 @@
 package edu.tongji.backend;
 
+import cn.hutool.core.date.DateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -15,6 +16,7 @@ import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 
 @Slf4j
 @Order(-1)
@@ -28,6 +30,18 @@ public class LogFilter implements GlobalFilter {
         String ip = request.getRemoteAddress().getAddress().getHostAddress();
         RequestPath path = request.getPath();
         stringRedisTemplate.opsForHyperLogLog().add("ip:addr:"+LocalDate.now().toString(),ip);
+        int dayOfMonth = LocalDate.now().getDayOfMonth();
+        int monthNum= LocalDate.now().getMonthValue();
+        int yearNum= LocalDate.now().getYear();
+        if(dayOfMonth==1){
+            Long size = stringRedisTemplate.opsForHyperLogLog().size("ip:addr:" + yearNum + "-" + monthNum);
+            if(size==null) {
+                for (int i = 1; i <= 30; i++) {
+                    stringRedisTemplate.opsForHyperLogLog().union("ip:addr:" + yearNum + "-" + monthNum,
+                            "ip:addr:" + LocalDate.now().minusDays(i).toString());
+                }
+            }
+        }
         log.info(ip+" had visited "+path+" at "+ LocalDateTime.now().toString());
         return chain.filter(exchange);
     }
