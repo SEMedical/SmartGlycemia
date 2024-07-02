a1a15d2081364ca4a503319355eb01c6e03d6e69
Victor Hu <victorhu493@gmail.com>
2024-07-02 21:44:20 +0800

    License

--- common-api/src/main/java/edu/tongji/backend/util/RegexPatterns.java
+++ common-api/src/main/java/edu/tongji/backend/util/RegexPatterns.java
@@ -1,5 +1,30 @@
 package edu.tongji.backend.util;
 
+/*-
+ * #%L
+ * Tangxiaozhi
+ * %%
+ * Copyright (C) 2024 Victor Hu
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+
+
 public abstract class RegexPatterns {
     public static final String IP_REGEX="\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}";
     public static final String EMAIL_REGEX="\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
